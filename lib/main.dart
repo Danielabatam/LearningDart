258cde1948eee9ae8a6b0fac34a577458977d032
Daniela BATAM <danielabatam@gmail.com>
2025-01-31 04:59:07 +0100

    [31-01-2025] Day 3: *Sound null safety

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,54 @@ void main() {
 }
 
 void test() {
-  const person = {
-    'age': 30,
-    'name': 'John Doe',
-  };
-  print(person);
-  person['name'] = 'FOOOOO';
-  print(person);
+  // const person = {
+  //   'age': 30,
+  //   'name': 'John Doe',
+  // };
+  // print(person);
+  // person['name'] = 'FOOOOO';
+  // print(person);
 
-  person['lastname'] = 'Baz';
-  print(person);
+  // person['lastname'] = 'Baz';
+  // print(person);
+
+  // --------------
+
+  // String? name = null;
+  // print(name);
+  // name = 'John Doe';
+  // print(name);
+
+  // --------------
+  // int? age = 20; {}
+
+  // --------------
+
+  // final List<int?>? numbers = [1, 2, 3, 4, 5];
+
+  //-----  Pick the first non null value || operator ??
+
+  // const String? firstName = null;
+  // const String? middleName = 'Bar';
+  // const String? lastName = 'Doe';
+
+  // const firstNonNullValue = firstName ?? middleName ?? lastName; // si la 1ere variable est nulle on passe à la suivante jsuqu'a trouver une valeur non nulle
+
+  // -------------- Null-aware assignement operator || ??= operator --------------------
+  // Cet operateur se rassure qu'une variable est non nulle avant de lui assigner une valeur
+  // on mets en parametre de la fonction void (String? firstName, String? middleName, String? lastName)
+  //par conséquent on les mets parametres dans test également ligne 58. ||     test(null, 'foe', 'bar');
+
+  // String? name = firstName;
+  // name ??= lastName; // si name est null on lui assigne lastName
+  // name ??= middleName; // si name est toujours null on lui assigne middleName
+  // print(name);
+
+  // -------------- Conditional Innvocation || ?. operator --------------------
+  // void test(List<String>? names) {
+  //   names?.add('Baz');
+  // }
+  //url: dart.dev/null-safety/u,derstanding-null-safety
 
 }
 
